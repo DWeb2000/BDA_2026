5be122ff7bc866ab1f057459e8485c478aa93988
DWeb2000 <danweber00@yahoo.fr>
2026-06-12 12:16:29 +0200

    Delete src directory

--- src/1_Read_matrix.R
+++
@@ -1,9 +0,0 @@
-###############################################################################
-# READ THE MATRIX "owl_matrix_full"
-###############################################################################
-
-# Main question project : 
-#Do barn owls and little owls occupy the same ecological niches in Italy?
-
-owl <- read.csv("data/owl_matrix_full.csv")
-
